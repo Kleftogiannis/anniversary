b55e9b7af02daaebbcf888124548a229a899e2c2
Kleftis <kleftojohn@gmail.com>
2025-11-15 01:10:52 +0200

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/anniversary', // Change this to '/your-repo-name/' if deploying to GitHub Pages
   plugins: [react(), tailwindcss()],
   
   build: {
